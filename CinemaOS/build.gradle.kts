7f39c15f9ce2ae792e9ac6b755ca62a943e9165b
PRAKASH000000 <126885477+PRAKASH000000@users.noreply.github.com>
2026-08-08 10:44:52 +0530

    Create build.gradle.kts

---
+++ CinemaOS/build.gradle.kts
@@ -0,0 +1,11 @@
+extra["extName"] = "CinemaOS"
+extra["pkgName"] = "com.cinemaos.tv"
+extra["extVersionCode"] = 1
+
+apply(from = "$rootDir/build.gradle.kts")
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += listOf("-Xskip-metadata-version-check")
+    }
+}
